fbf636098b64def5aa18a0f289a0c83a56d852ef
Dan Lans <dslans@gmail.com>
2019-10-17 21:49:44 -0500

    added dialogue elements

---
+++ dialogue/lumpia_recipe.py
@@ -0,0 +1,11 @@
+recipe_ingredients = [
+    ("Lumpia Wrappers", "1 pack"),
+    ("Ground beef", "1lb"),
+    ("Garlic", "3 cloves"),
+    ("Onion", "1"),
+    ("Sweet Pickle Relish", "1/4 cup"),
+    ("Diced Potatoes", "1 cup"),
+    ("Diced Carrots", "1/2 cup"),
+    ("Egg for egg wash", "1"),
+    ("Oil for frying", "1")
+    ]
